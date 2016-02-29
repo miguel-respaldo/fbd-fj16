293493381ecfdb0665ac4932e88ccd5a4a095cb1
Miguel Bernal Marin <mbernal@itszapopan.edu.mx>
2016-02-29 08:30:58 -0600

    No se agrego nada

--- clase/HolaMundo.java
+++ clase/HolaMundo.java
@@ -2,7 +2,7 @@ public class HolaMundo {
 
   public static void main(String []args) {
     System.out.println("Hola Mundo en Java");
-    System.out.println("Otra cosa");
+    System.out.println("Se les ocurre Nada");
   }
 
 }
